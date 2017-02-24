16053f7afb616222787e93cd8a63d3a4d4fd4519
DWS-paris <j.noyer@digitalworkshop.fr>
2017-02-24 16:32:47 +0100

    Ajout du portfolio ajax

--- HTMLsite/js/script.js
+++ HTMLsite/js/script.js
@@ -13,5 +13,56 @@ $(document).ready(function(){
         // Version courte : burgerIcon.click(function(){burgerMenu.slideToggle()})
 
 
+    // Portfolio
+        // Vérifier la présence de la class pagePortfolio
+        if($('.pagePortfolio').length == 1){
+
+            console.log('Vous êtes sur la page portfolio');
+
+            // Créer une fonction pour ajouter des balises dans le DOM
+            function addContent(cover, title, subtitle){
+                $('main section').append(''+
+                
+                    '<article>' +
+                        '<button>Voir le projet</button>' +
+                        '<div>' +
+                            '<h3>'+ title +'</h3>' +
+                            '<p>'+ subtitle +'</p>' +
+                        '</div>' +
+                        '<span style="background-image: url(img/'+ cover +')"></span>'+
+                    '</article>'
+                );
+            };
+
+            $.ajax({
+
+                // Header de la requête
+                    url: 'data/portfolio.json',
+                    type: 'GET',
+                    dataType: 'JSON',
+
+                // Corps de la requête
+                    success: function(data){
+                        
+                        // Faire une boucle sur data
+                        for( var i = 0; i < data.length; i++ ){
+                            console.log(data[i]);
+                            addContent(data[i].cover, data[i].title, data[i].subtitle)
+                        }
+
+                    },
+                    error: function(err){
+                        console.log(err)
+                    }
+
+            })
+        }
+
+
+    
+    
+
+
+
 
 }); // Fin de la fonction d'attente du chargement du DOM
